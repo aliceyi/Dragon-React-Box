30d2c409e8d29cda6c37de7782247b3d3624b887
alice.yi <alice.yi@farfetch.com>
2021-03-12 17:40:21 +0800

    chore(button): add test props only test

--- packages/dragon-button/src/index.tsx
+++ packages/dragon-button/src/index.tsx
@@ -50,7 +50,7 @@ export interface BaseButtonProps {
     /**
      * only for test and after need remove
      */
-    onlyTest1?: string
+    onlyTest2?: string
     /**
      * setting button click callback
      */
