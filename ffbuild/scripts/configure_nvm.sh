beb0ceec196ece70eaca9a060e1097d3e01d63c7
alice <5541037@qq.com>
2021-04-06 23:53:08 +0800

    chore(ffbuild): remove ffbuild.yaml

--- ffbuild/scripts/configure_nvm.sh
+++ ffbuild/scripts/configure_nvm.sh
@@ -2,7 +2,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-export NODE_VERSION=12.20.1
+export NODE_VERSION=12.15.0
 
 nvm install ${NODE_VERSION}
 nvm use ${NODE_VERSION}
