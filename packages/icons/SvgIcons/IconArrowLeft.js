eecd280d30a83894fca4bffe004caa10f70af03a
alice <5541037@qq.com>
2021-04-20 08:58:18 +0800

    chore(icons): add icons

---
+++ packages/icons/SvgIcons/IconArrowLeft.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+function SvgIconArrowLeft(props) {
+    return (
+        <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path d="M15.1 18.48L7 12l8.1-6.48v2.56L10.2 12l4.9 3.92v2.56z" fill="#222" />
+        </svg>
+    )
+}
+
+export default SvgIconArrowLeft
---
+++ packages/icons/SvgIcons/IconArrowUp.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+function SvgIconArrowUp(props) {
+    return (
+        <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path d="M18.48 15L12 6.899 5.519 15H8.08L12 10.1l3.92 4.9h2.56z" fill="#222" />
+        </svg>
+    )
+}
+
+export default SvgIconArrowUp
